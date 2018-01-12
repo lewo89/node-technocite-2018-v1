487657adc82d4d41f87fae4b2d9352ab11c94c9f
lewo89 <leonoredelporte@gmail.com>
2018-01-12 16:35:54 +0100

    exercice4-2

--- exe04/routes/amis.js
+++ exe04/routes/amis.js
@@ -1,7 +1,32 @@
 const fs = require('fs')
-module.exports = (req,res)=>{
-    fs.readFile(`${process.cwd()}/template/amis.html`,{encoding:'utf-8'},(err,data)=>{
-        res.end(data)
-    })
+module.exports = (req,res) =>{
+    fs.readFile(`${process.cwd()}/public/friends.json`, { encoding: 'utf-8' }, (err, json) => {
+        if(err) {
+            res.writeHead(500,{'Content-Type':'text/html'})
+            res.end('The server  has a problem please try later')
+        } else {
+            fs.readFile(`${process.cwd()}/templates/amis.html`, { encoding: 'utf-8' }, (err, data) => {
+                if(err) {
+                    res.writeHead(404,{'Content-Type':'text/html'})
+                    res.end('File Not found')
+                } else{
+                    res.end(generateHtml(data,json))
+                }
 
+            })
+        }
+      
+    })
+}
+
+const generateHtml = (tpl,json) =>{
+    // let values = JSON.parse(json)
+    // let tempArr = values.map(item =>item.name)
+    // console.log(tempArr)
+    // let tempStr = tempArr.join('</li><li>')
+    // console.log(tempStr)
+    // let htmlStr = tpl.replace('%friends%',tempStr)
+    // console.log(htmlStr)
+    // return htmlStr
+    return tpl.replace('%friends%',JSON.parse(json).map(item =>item.name).join('</li><li>'))
 }
